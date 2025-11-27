7f37af37869409c742cbf4816f2f845cd1598fc7
Markus Åkerlund Larsson <markus.akerlund.larsson@umu.se>
2025-11-27 23:11:56 +0100

    removed unused code

--- enemy_spec.gd
+++ enemy_spec.gd
@@ -3,11 +3,9 @@ extends Resource
 
 @export var name: StringName
 @export var tags: Dictionary[Enemy.Tag, int]
-@export var tags_s: Dictionary[StringName, int]
 @export var color: Color
 
 func _init(p_name = "", p_tags: Dictionary[Enemy.Tag, int] = {}, p_color = Color.WEB_GRAY):
 	name = p_name
 	tags = p_tags
 	color = p_color
-	tags_s = {}
